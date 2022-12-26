35687ef2c9a1b980bb1a3e08665fad5c0abdf928
Ashim Singh <ashimsingh948@gmail.com>
2022-12-26 21:03:43 +0545

    challange complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='parent'>
+      <div className='child'>
+        <div className='box1'>
+          <h2>
+            The Nepali Commerce Platform
+          </h2>
+          <p>
+            Build your business with InternetPasal to sell online, offline, and everywhere in between.
+          </p>
+          <div className='box1__btns'>
+            <button className='btn'>Try it for free</button>
+            <button className='btn --2'>Watch the video</button>
+          </div>
+        </div>
+        <div className='box2'>
+          <img src="https://www.rmg.co.uk/sites/default/files/styles/gallery_fullscreen/public/OM29130307950_A%20Titanium%20Moon%20%C2%A9%20Miguel%20Claro.jpg?itok=0QMgmG5I"></img>
+          
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
